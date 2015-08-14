5278082e5d38581e49ff83cb37c846a3f0d74c4a
Diego Vidal <diefervidal91@gmail.com>
2015-08-14 15:13:54 -0500

    First Commit2

---
+++ TaxiBuddy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaxiBuddy
+//
+//  Created by Developer on 14/08/15.
+//  Copyright © 2015 iAm Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
